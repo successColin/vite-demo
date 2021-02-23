a9425c2bf1f9adaca0e790ae0b23f731b34fcd27
13067565776@163.com <13067565776@163.com>
2021-02-23 09:31:29 +0800

    使用vscode 提交 git

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
   <!-- <div class="nav-btn">
     <router-link to='/' class="contact">Contact </router-link>
   </div> -->
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
 </template>
 
 <script>
